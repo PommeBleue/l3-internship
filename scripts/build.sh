76e1812f6305eb452e4bcecab5edeaa82655d814
Amar Ahmane <tiplouf@machine.local>
2024-12-29 02:56:38 +0100

    Updating build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,8 @@
 
 xelatex -output-directory="./output" "./main.tex"
 
-if [[ $1 == "del" ]]; then 
-    rm -vf ./output/*.log ./output/*.aux ./output/*.idx ./output/*.toc ./output/*.synctex.gz ./output/*.out ./output/*.dvi ./output/*.fdb_latexmk ./output/*.fls ./output/*.ilg ./output/*.ind ./output/*.pgf-plot.gnuplot
 elif [[ $1 == "bib" ]]; then 
     bibtex output/main
     xelatex -output-directory="./output" "./main.tex"
-else
-    echo "Argument specified not recognized"
 fi
 
